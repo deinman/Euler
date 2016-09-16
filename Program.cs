458cbca6e0724dc6a45c0ae05c137bf9a03efb8f
Daniel Inman <danielinman@Daniels-MacBook-Pro.local>
2016-09-16 01:00:18 -0400

    Completed problem2
    Problem 2 confirmed completed.
    Updated some vscode settings.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace Euler
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TO-DO: User input 
+            // Console.WriteLine("Choose a problem!");
+            // string input = Console.ReadLine();
 
+            Console.WriteLine("Problem1:");
             EulerLib.Problem1();
+            Console.WriteLine();
+
+            Console.WriteLine("Problem2:");
+            EulerLib.Problem2();
+            Console.WriteLine();
 
             Console.ReadLine();
         }
