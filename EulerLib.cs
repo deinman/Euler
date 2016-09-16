811c5688395eeffed8a80888c5643f61519ca107
Daniel Inman <danielinman@Daniels-MacBook-Pro.local>
2016-09-16 01:09:19 -0400

    Adding Problem3 Framework

--- EulerLib.cs
+++ EulerLib.cs
@@ -61,5 +61,15 @@ namespace Euler
 
             Console.WriteLine(termsSum);
         }
+
+        public static void Problem3()
+        {
+            /*
+            The prime factors of 13195 are 5, 7, 13 and 29.
+            What is the largest prime factor of the number 600851475143 ?
+            */
+
+            Console.WriteLine("The largest prime factor of 600851475143");
+        }
     }
 }
