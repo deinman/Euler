db6f52e028f057f2e319d79c86b5bf168a142552
Daniel Inman <danielinman@Daniels-MacBook-Pro.local>
2016-09-16 00:03:24 -0400

    Initial Commit
    DotNet Core components and Project1 complete.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Euler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            EulerLib.Problem1();
+
+            Console.ReadLine();
+        }
+    }
+}
